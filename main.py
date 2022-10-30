50a6e908c362106a14bb44a801370429e433374d
Li Gu <ligu2006@gmail.com>
2022-10-30 19:46:01 -0400

    add redis

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+import time
 from flask import Flask, jsonify
 import os
+import quickfix as qf
+import redis
 
+#cache = redis.Redis(host='redis', port=6379)
 app = Flask(__name__)
 
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+#    count = get_hit_count()
+    #return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return json.dumps(dir(qf))
 
 
 if __name__ == '__main__':
